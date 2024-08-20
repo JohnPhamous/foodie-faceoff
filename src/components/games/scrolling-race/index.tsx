aafd3e7f44c58409193b5abb79f354a09e8a2292
John Pham <johnphammail@gmail.com>
2024-08-20 14:49:02 -0700

    fsfdsf

--- src/components/games/scrolling-race/index.tsx
+++ src/components/games/scrolling-race/index.tsx
@@ -3,56 +3,64 @@ import { useShakeCounter } from "@/app/hooks/use-device-shake";
 import { useStopwatch } from "@/app/hooks/use-stopwatch";
 import { Button } from "@/components/button";
 import { useMyPresence } from "@liveblocks/react/suspense";
-import { useEffect, useState } from "react";
-
-const NUMBER_OF_SHAKES = 8;
+import { useScroll } from "framer-motion";
+import { useEffect, useRef, useState } from "react";
 
 export const Scrolling = () => {
-  const { shakeCount, resetCount, requestPermission, isListening } =
-    useShakeCounter();
-  const [startTime, setStartTime] = useState(0);
   const { elapsedTime, isRunning, start, stop, reset } = useStopwatch();
+
+  const carouselRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    container: carouselRef,
+  });
 
   const [myPresence, setMyPresence] = useMyPresence();
 
   useEffect(() => {
-    if (isListening) {
-      setStartTime(Date.now());
-      start();
-    }
-  }, [isListening, start]);
+    start();
+  }, [start]);
 
-  const gameDone = shakeCount >= NUMBER_OF_SHAKES;
-
+  const progress = scrollYProgress.get();
   useEffect(() => {
-    if (shakeCount === NUMBER_OF_SHAKES) {
+    if (progress <= 1) {
+      setMyPresence({ progress });
+    }
+
+    if (progress === 1 && isRunning) {
       setMyPresence({
         score: elapsedTime,
       });
       stop();
     }
-    if (shakeCount <= NUMBER_OF_SHAKES) {
-      setMyPresence({
-        progress: shakeCount / NUMBER_OF_SHAKES,
-      });
-    }
-  }, [elapsedTime, setMyPresence, shakeCount, startTime, stop]);
+  }, [elapsedTime, isRunning, progress, setMyPresence, stop]);
+
+  const gameDone = progress === 1;
 
   return (
-    <div className="h-full">
-      {!isListening && (
-        <Button onClick={requestPermission}>Request Permission</Button>
-      )}
-      {isListening && !gameDone && (
-        <div className="flex flex-col justify-between h-full text-center">
-          <h2 className="bg-[#FFC530] px-2">SHAKE YOUR PHONE!!</h2>
-          <h3 className="text-[300px]">
+    <div className="h-full w-full">
+      {!gameDone && (
+        <div className="flex flex-col justify-between items-center h-full text-center">
+          <h2 className="bg-[#FFC530] px-2 w-fit">START SCROLLING!!</h2>
+          <h3>{(progress * 100).toFixed(0)}%</h3>
+          <div ref={carouselRef} className="w-full overflow-auto h-[400px]">
+            {Array.from({ length: 250 }).map((_, index) => (
+              <div
+                key={index}
+                style={{
+                  backgroundColor: `hsl(${(index * 18) % 360}, 100%, 50%)`,
+                  height: "700px",
+                  width: "100%",
+                }}
+              />
+            ))}
+          </div>
+          {/* <h3 className="text-[300px]">
             {NUMBER_OF_SHAKES - shakeCount > 0 ? (
               <>{NUMBER_OF_SHAKES - shakeCount}</>
             ) : (
               <>0</>
             )}
-          </h3>
+          </h3> */}
           <p>{elapsedTime.toFixed(0)}s</p>
         </div>
       )}
